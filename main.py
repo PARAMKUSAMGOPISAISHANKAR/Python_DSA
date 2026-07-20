f6ed09d8f823fd78bf1a7652d818ca13b35ad6b5
PARAMKUSAMGOPISAISHANKAR <p.gopisaishankar2479@gmail.com>
2026-07-20 12:13:04 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,63 @@
+# print("Gopi Sai Shankar\n")
+# print(r"\tcurrent\new\folder")\
+
+
+
+# def check(num):
+#     if num % 2 == 0:
+#         print("Even")
+#     else:
+#         print("Odd")
+        
+# check(10)
+
+
+# l=[1, 2, 3, 4, 5]
+# l.pop(2)
+# print(l)
+# dict={
+# "name":"Gopi Sai Shankar",
+# "gender":"Male",
+# "age":21,
+# "course":["Python","java","DS"]
+# }
+    
+# dict.update({"name":"Gopi Sai Shankar",})
+# print(dict)
+# print(dict["name"])
+# print(dict.get("name"))
+# print(dict.keys())
+# print(dict.values())
+# print(dict.items())
+
+
+# def count(*args):
+#     print(type(args))
+# # count(1,2,3,4,5,)
+# def dicts(**kwargs):
+#     print(type(kwargs))
+# dicts(name="Gopi Sai Shankar", age=21, gender="Male")
+# def default(gender,age,name="Gopi Sai Shankar",):
+#     print(name,age,gender)
+
+# x=10
+# x="data"
+# print(x)
+
+
+# x="Gopi Sai Shankar"
+# x=x[1:-1]
+# x=x[::-1]
+# print(x)
+# print(x[1:3])
+
+# while True:
+#     print("Hi")
+
+
+x = int(input())
+sum = 0
+for i in range(1, x):
+    sum += i
+print(sum)
+    
